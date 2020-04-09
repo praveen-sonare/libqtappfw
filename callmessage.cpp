praveen-sonare/libqtappfw

commit 0ed292d3ccf93c889734960676a321d1166d3f66

rework message hierarchy
Rework message hierarchy with the final objective
of splitting libqtappfw into several libraries.
This commit carries the following changes:
- Simplify message hierarchy, keeping abstract
Message class, adding specialization for call and
event messages, keeping ResponseMessage, and
removing all module specific specializations.
- Add MessageFactory class to create message
objects.
- Change messages life cycle: using smart pointers
and removing QObject from message hierarchy (a
Message is not a QObject anymore and thus
'deleteLater()' is not available).
- Adapt all modules to use new message hierarchy.
- Keep ResponseMessage original constructor to
avoid breaking TaskManager.
- Message constructors have been kept public, but
will go private on a follow-up patch (once
TaskManager class has been modified to use new
MessageFactory).

Bug-AGL: SPEC-3112
Signed-off-by: Raquel Medina <raquel.medina@konsulko.com>
Change-Id: I3a7a6325209ddeca2293f1ac745371861a947bfb

callmessage.cpp
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2020 Konsulko Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <QDebug>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonValue>
+
+#include "callmessage.h"
+#include "message.h"
+
+
+bool CallMessage::createRequest(QString api, QString verb, QJsonValue parameter)
+{
+	if (!m_request.isEmpty()){
+		qWarning("Message instance has already been used. Cannot send another request.");
+		return false;
+	}
+
+	m_request["msgid"] = static_cast<unsigned int>(MessageId::Call);
+	m_request["callid"] = 0;
+	m_request["api"] = api;
+	m_request["verb"] = verb;
+	m_request["parameter"] = parameter;
+
+	m_init = true;
+
+	return m_init;
+}
+
+QByteArray CallMessage::serialize(QJsonDocument::JsonFormat format)
+{
+	QJsonArray array;
+	array.append(m_request["msgid"].toInt());
+	array.append(m_request["callid"].toInt());
+	array.append(m_request["api"].toString() + "/" + m_request["verb"].toString());
+	array.append(m_request["parameter"].toJsonValue());
+
+	QJsonDocument jdoc;
+	jdoc.setArray(array);
+
+	return jdoc.toJson(format).data();
+}

message.h
@@ -23,103 +23,44 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 
-enum MessageId {
+enum class MessageId {
+	Invalid = 0,
 	Call = 2,
 	RetOk = 3,
 	RetErr = 4,
 	Event = 5,
 };
 
-enum class MessageType {
-	GenericMessage,
-	ResponseRequestMessage,
-	TelephonyEventMessage,
-	WeatherEventMessage,
-	MediaplayerEventMessage,
-	NetworkEventMessage,
-	BluetoothEventMessage,
-	PbapEventMessage,
-	RadioEventMessage,
-	MapEventMessage,
-	NavigationEventMessage,
-	VoiceEventMessage,
-	SignalComposerEventMessage,
-	GuiMetadataCapabilityEventMessage,
-	HVACEventMessage,
-};
+class QWebSocket;
 
-class Message : public QObject
+class Message
 {
-	Q_OBJECT
-	Q_ENUM(MessageId)
-
 	public:
 		Message();
+		void  setAdditionalData(QByteArray data) {};
+		QByteArray send(QWebSocket& transport, unsigned int callid);
 
-		bool fromJson(QByteArray jsonData);
-		virtual bool fromJDoc(QJsonDocument jdocData);
-		QByteArray toJson(QJsonDocument::JsonFormat format = QJsonDocument::Compact);
-		bool createRequest(QString api, QString verb, QJsonValue parameter = "None");
-		inline QString eventApi() const
-		{
-			return m_event_api;
-		}
-
-		inline QString eventName() const
-		{
-			return m_event_name;
-		}
-
-		inline QJsonObject eventData() const
-		{
-			return m_event_data;
-		}
-
-		inline QString replyStatus() const
-		{
-			return m_reply_status;
-		}
-
-		inline QString replyInfo() const
-		{
-			return m_reply_info;
-		}
-
-		inline QJsonObject replyData() const
-		{
-			return m_reply_data;
-		}
-
-		inline bool isEvent() const
-		{
-			return m_event;
-		}
-
-		inline bool isReply() const
-		{
-			return m_reply;
-		}
-
-		inline bool isValid() const
+		inline bool isComplete() const
 		{
 			return m_init;
 		}
 
-		inline void setCallId(qint32 callId) {
-			m_request["callid"] = callId;
+		virtual bool getCallId(unsigned int *id) const
+		{
+			return false;
 		}
 
-		inline QMap<QString, QVariant> requestData() const
-		{
-			return m_request;
-		}
+		static MessageId isValid(QJsonDocument );
+
+		virtual bool isEvent() = 0;
+		virtual bool isReply() = 0;
 
 	protected:
-		bool m_event, m_init, m_reply;
-		QString m_event_api, m_event_name, m_reply_info, m_reply_status, m_reply_uuid;
-		QMap<QString, QVariant> m_request;
+		virtual void updateCallId(unsigned int id) {};
+		virtual QByteArray serialize(QJsonDocument::JsonFormat format = QJsonDocument::Compact);
+
+		bool m_init;
 		QJsonDocument m_jdoc;
-		QJsonObject m_event_data, m_reply_data;
 };
 
 #endif // MESSAGE_H

responsemessage.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Konsulko Group
+ * Copyright (C) 2018-2020 Konsulko Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,28 +22,105 @@
 
 #include "responsemessage.h"
 
+//deprecated method call new constructor and  setAdditionalData() instead:
 ResponseMessage::ResponseMessage(QByteArray request)
 {
 
-    QJsonDocument jdoc(QJsonDocument::fromJson(request));
+	QJsonDocument jdoc(QJsonDocument::fromJson(request));
 
-    if (!jdoc.isArray()) {
-        qWarning("Invalid appfw message: not an array");
-        return;
-    }
+	if (!jdoc.isArray()) {
+		qWarning("Invalid appfw message: not an array");
+		return;
+	}
 
-    QJsonArray msg = jdoc.array();
+	QJsonArray msg = jdoc.array();
 
-    if (msg.size() != 4) {
-        qWarning("Invalid appfw message: invalid array size");
-        return;
-    }
+	if (msg.size() != 4) {
+		qWarning("Invalid appfw message: invalid array size");
+		return;
+	}
 
-    QStringList api_str_list = msg[2].toString().split(QRegExp("/"));
+	QStringList api_str_list = msg[2].toString().split(QRegExp("/"));
 
-    m_request["msgid"] = msg.at(0);
-    m_request["callid"] = msg.at(1);
-    m_request["api"] = api_str_list[0];
-    m_request["verb"] = api_str_list[1];
-    m_request["parameter"] = msg.at(3);
+	m_request["msgid"] = msg.at(0);
+	m_request["callid"] = msg.at(1);
+	m_request["api"] = api_str_list[0];
+	m_request["verb"] = api_str_list[1];
+	m_request["parameter"] = msg.at(3);
+}
+
+ResponseMessage::ResponseMessage(QJsonDocument content)
+{
+	QJsonArray msg = content.array();
+	if (!msg[2].isObject()) {
+		qWarning("Invalid appfw payload: no JSON object");
+		return;
+	}
+
+	//deserialize:
+	auto callid = msg[1].toString().toInt();
+	QJsonObject payload = msg[2].toObject();
+
+	auto request_iter = payload.find("request");
+	auto request = request_iter.value().toObject();
+	if (request.empty()) {
+		qWarning("Invalid appfw reply message: empty request data");
+		return;
+	}
+
+	auto status_iter = request.find("status");
+	auto info_iter = request.find("info");
+	auto response_iter = payload.find("response");
+	auto response = response_iter.value().toObject();
+	m_reply_status = status_iter.value().toString();
+	m_reply_info = info_iter.value().toString();
+	m_reply_data = response;
+	m_reply_callid = callid;
+	m_init = false; //not complete yet, missing matching request data
+}
+
+bool ResponseMessage::setAdditionalData(QByteArray data)
+{
+	QJsonDocument jdoc(QJsonDocument::fromJson(data));
+	if (!jdoc.isArray()) {
+		qWarning("Invalid data: not an array");
+		return false;
+	}
+
+	QJsonArray content = jdoc.array();
+	if (content.size() != 4) {
+		qWarning("Invalid data: invalid array size");
+	return false;
+	}
+
+	QStringList api_str_list = content[2].toString().split(QRegExp("/"));
+	m_request["msgid"] = content.at(0);
+	m_request["callid"] = content.at(1);
+	m_request["api"] = api_str_list[0];
+	m_request["verb"] = api_str_list[1];
+	m_request["parameter"] = content.at(3);
+	m_init = true;
+	return true;
+}
+
+bool ResponseMessage::copyCallId(unsigned int *id)
+{
+	*id = m_reply_callid;
+	return true;
+}
+
+QByteArray ResponseMessage::serialize(QJsonDocument::JsonFormat format)
+{
+	QJsonArray array;
+	(m_reply_status == "failed")?
+		array.append(static_cast<int>(MessageId::RetErr)) :
+		array.append(static_cast<int>(MessageId::RetOk));
+	array.append(static_cast<int>(m_reply_callid));
+	array.append(m_request["api"].toString() + "/" + m_request["verb"].toString());
+	array.append(m_reply_data);
+
+	QJsonDocument jdoc;
+	jdoc.setArray(array);
+
+	return jdoc.toJson(format).data();
 }

responsemessage.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Konsulko Group
+ * Copyright (C) 2018-2020 Konsulko Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,26 +17,69 @@
 #ifndef RESPONSEMESSAGE_H
 #define RESPONSEMESSAGE_H
 
-#include <QObject>
 
 #include "message.h"
 
 class ResponseMessage : public Message
 {
-    Q_OBJECT
 
-    public:
-        explicit ResponseMessage(QByteArray request = nullptr);
 
-        inline QString requestVerb() const
-        {
-            return m_request["verb"].toString();
-        }
+	public:
+		//deprecated:
+		explicit ResponseMessage(QByteArray request = nullptr);
 
-        inline QVariantMap requestParameters() const
-        {
-            return m_request["parameter"].toMap();
-        }
+		explicit ResponseMessage(QJsonDocument data);
+
+		inline QString requestApi() const
+		{
+			return m_request["api"].toString();
+		}
+
+		inline QString requestVerb() const
+		{
+			return m_request["verb"].toString();
+		}
+
+		inline QVariantMap requestParameters() const
+		{
+			return m_request["parameter"].toMap();
+		}
+
+		inline QString replyStatus() const
+		{
+			return m_reply_status;
+		}
+
+		inline QString replyInfo() const
+		{
+			return m_reply_info;
+		}
+
+		inline QJsonObject replyData() const
+		{
+			return m_reply_data;
+		}
+
+		bool isEvent() override
+		{
+			return false;
+		}
+
+		bool isReply() override
+		{
+			return true;
+		}
+
+		bool setAdditionalData(QByteArray data);
+		bool copyCallId(unsigned int *id);
+
+		QByteArray serialize(QJsonDocument::JsonFormat format = QJsonDocument::Compact) override;
+
+	private:
+		QString m_reply_info, m_reply_status, m_reply_uuid;
+		unsigned int m_reply_callid;
+		QJsonObject m_reply_data;
+		QMap<QString, QVariant> m_request;
 };
 
 #endif // RESPONSEMESSAGE_H

CMakeLists.txt
@@ -61,8 +61,8 @@ set (SUBDIRS
 	voice-capabilities
 	weather)
 
-add_headers(message.h messageengine.h responsemessage.h)
-add_sources(message.cpp messageengine.cpp responsemessage.cpp)
+add_headers(message.h messagefactory.h messageengine.h responsemessage.h callmessage.h eventmessage.h)
+add_sources(message.cpp messagefactory.cpp messageengine.cpp responsemessage.cpp callmessage.cpp eventmessage.cpp)
 
 foreach(subdir ${SUBDIRS})
 	add_subdirectory(${subdir})

bluetooth/CMakeLists.txt
@@ -1,2 +1,2 @@
-add_headers(bluetooth.h bluetoothmessage.h bluetoothmodel.h)
-add_sources(bluetooth.cpp bluetoothmessage.cpp bluetoothmodel.cpp)
+add_headers(bluetooth.h bluetoothmodel.h)
+add_sources(bluetooth.cpp bluetoothmodel.cpp)

bluetooth/bluetooth.cpp
@@ -16,13 +16,13 @@
 
 #include <QDebug>
 
-#include "message.h"
-#include "bluetoothmessage.h"
+#include "callmessage.h"
+#include "eventmessage.h"
 #include "responsemessage.h"
+#include "messagefactory.h"
 #include "messageengine.h"
-#include "bluetoothmodel.h"
 #include "bluetooth.h"
-
+#include "bluetoothmodel.h"
 
 
 Bluetooth::Bluetooth (QUrl &url, QQmlContext *context, QObject * parent) :
@@ -58,10 +58,13 @@ Bluetooth::~Bluetooth()
 
 void Bluetooth::send_command(QString verb, QJsonObject parameter)
 {
-    BluetoothMessage *tmsg = new BluetoothMessage();
-    tmsg->createRequest(verb, parameter);
-    m_mloop->sendMessage(tmsg);
-    delete tmsg;
+    std::unique_ptr<Message> msg = MessageFactory::getInstance().createOutboundMessage(MessageId::Call);
+    if (!msg)
+        return;
+
+    CallMessage* btmsg = static_cast<CallMessage*>(msg.get());
+    btmsg->createRequest("Bluetooth-Manager", verb, parameter);
+    m_mloop->sendMessage(std::move(msg));
 }
 
 void Bluetooth::setPower(bool state)
@@ -261,32 +264,39 @@ void Bluetooth::processAdapterChangesEvent(QJsonObject data)
         m_discoverable = false;
 }
 
-void Bluetooth::onMessageReceived(MessageType type, Message *msg)
+void Bluetooth::onMessageReceived(std::shared_ptr<Message> msg)
 {
-    if (msg->isEvent() && type == MessageType::BluetoothEventMessage) {
-        BluetoothMessage *tmsg = qobject_cast<BluetoothMessage*>(msg);
+    if (!msg)
+        return;
 
-        if (tmsg->isDeviceChangesEvent()) {
-            processDeviceChangesEvent(tmsg->eventData());
-        } else if (tmsg->isAdapterChangesEvent()) {
-            processAdapterChangesEvent(tmsg->eventData());
-        } else if (tmsg->isAgentEvent()) {
-            emit requestConfirmationEvent(tmsg->eventData());
+    if (msg->isEvent()) {
+        std::shared_ptr<EventMessage> emsg = std::static_pointer_cast<EventMessage>(msg);
+        QString ename = emsg->eventName();
+        QString eapi = emsg->eventApi();
+        QJsonObject data = emsg->eventData();
+        if (eapi != "Bluetooth-Manager")
+            return;
+        if (ename == "device_changes") {
+            processDeviceChangesEvent(data);
+        } else if (ename == "adapter_changes") {
+            processAdapterChangesEvent(data);
+        } else if (ename == "agent") {
+            emit requestConfirmationEvent(data);
         }
 
-    } else if (msg->isReply() && type == MessageType::ResponseRequestMessage) {
-        ResponseMessage *tmsg = qobject_cast<ResponseMessage*>(msg);
-
-        if (tmsg->requestVerb() == "managed_objects") {
-            populateDeviceList(tmsg->replyData());
-        } else if (tmsg->requestVerb() == "adapter_state") {
-            bool powered = tmsg->replyData().value("powered").toBool();
+    } else if (msg->isReply()) {
+        std::shared_ptr<ResponseMessage> rmsg = std::static_pointer_cast<ResponseMessage>(msg);
+        //get api name
+        QString verb = rmsg->requestVerb();
+        QJsonObject data = rmsg->replyData();
+        if (verb == "managed_objects") {
+            populateDeviceList(data);
+        } else if (verb == "adapter_state") {
+            bool powered = data.value("powered").toBool();
             if (m_power != powered) {
                 m_power = powered;
                 emit powerChanged(m_power);
             }
         }
     }
-
-    msg->deleteLater();
 }

bluetooth/bluetooth.h
@@ -17,16 +17,15 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
+#include <memory>
 #include <QObject>
-#include <QJsonArray>
 #include <QJsonObject>
+#include <QJsonArray>
 #include <QtQml/QQmlContext>
 
 class BluetoothModel;
 class MessageEngine;
 class Message;
-
-enum class MessageType;
 
 class Bluetooth : public QObject
 {
@@ -80,7 +79,7 @@ class Bluetooth : public QObject
         // slots
         void onConnected();
         void onDisconnected();
-        void onMessageReceived(MessageType, Message*);
+        void onMessageReceived(std::shared_ptr<Message>);
 
         QString process_uuid(QString uuid) { if (uuid.length() == 36) return uuid; return uuids.value(uuid); };
 

commit e57b4f75b55c2befad4f9944f43c8aa2310321b7

add missing overrides in ResponseMessage
The original message hierarchy rework  patch
is missing functionality which results in received
replies being discarded.
Basically, ResponseMessage class is
missing the override required to match replies to
the original calls.

Bug-AGL: SPEC-3112
Fixes: 0ed292d3ccf9 (rework Message hierarchy)

Signed-off-by: Raquel Medina <raquel.medina@konsulko.com>
Change-Id: I39030ef212b2175a590deca02f9f9ff41bf7abf4

callmessage.cpp
@@ -49,8 +49,7 @@ QByteArray CallMessage::serialize(QJsonDocument::JsonFormat format)
 	array.append(m_request["api"].toString() + "/" + m_request["verb"].toString());
 	array.append(m_request["parameter"].toJsonValue());
 
-	QJsonDocument jdoc;
-	jdoc.setArray(array);
+	m_jdoc.setArray(array);
 
-	return jdoc.toJson(format).data();
+	return m_jdoc.toJson(format).data();
 }

message.h
@@ -37,7 +37,10 @@ class Message
 {
 	public:
 		Message();
-		void  setAdditionalData(QByteArray data) {};
+		virtual bool  setAdditionalData(QByteArray data)
+		{
+			return false;
+		}
 		QByteArray send(QWebSocket& transport, unsigned int callid);
 
 		inline bool isComplete() const

responsemessage.cpp
@@ -22,35 +22,10 @@
 
 #include "responsemessage.h"
 
-//deprecated method call new constructor and  setAdditionalData() instead:
-ResponseMessage::ResponseMessage(QByteArray request)
-{
-
-	QJsonDocument jdoc(QJsonDocument::fromJson(request));
-
-	if (!jdoc.isArray()) {
-		qWarning("Invalid appfw message: not an array");
-		return;
-	}
-
-	QJsonArray msg = jdoc.array();
-
-	if (msg.size() != 4) {
-		qWarning("Invalid appfw message: invalid array size");
-		return;
-	}
-
-	QStringList api_str_list = msg[2].toString().split(QRegExp("/"));
-
-	m_request["msgid"] = msg.at(0);
-	m_request["callid"] = msg.at(1);
-	m_request["api"] = api_str_list[0];
-	m_request["verb"] = api_str_list[1];
-	m_request["parameter"] = msg.at(3);
-}
 
 ResponseMessage::ResponseMessage(QJsonDocument content)
 {
+	m_jdoc = content;
 	QJsonArray msg = content.array();
 	if (!msg[2].isObject()) {
 		qWarning("Invalid appfw payload: no JSON object");

responsemessage.h
@@ -60,6 +60,11 @@ class ResponseMessage : public Message
 			return m_reply_data;
 		}
 
+		bool getCallId(unsigned int *id) const override
+		{
+			*id = m_reply_callid;
+			return true;
+		}
 		bool isEvent() override
 		{
 			return false;
@@ -70,7 +75,7 @@ class ResponseMessage : public Message
 			return true;
 		}
 
-		bool setAdditionalData(QByteArray data);
+		bool setAdditionalData(QByteArray data) override;
 		bool copyCallId(unsigned int *id);
 
 		QByteArray serialize(QJsonDocument::JsonFormat format = QJsonDocument::Compact) override;